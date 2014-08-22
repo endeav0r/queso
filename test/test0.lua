4979c45b17b6b2e2d9a5a331961c85c4293eafbb
endeavor <endeavor@rainbowsandpwnies.com>
2014-08-21 21:47:25 -0500

    test changes

--- test/test0.lua
+++ test/test0.lua
@@ -3,8 +3,14 @@ require('lqueso')
 local test0 = lqueso.elf32('test0')
 
 local symbols = test0:symbols()
+for name, symbol in pairs(symbols) do
+    print(name, symbol)
+end
 
 local memoryModel = test0:memoryModel()
-local graph = lqueso.x86disassemble(test0:entry(), memoryModel)
+local graph = lqueso.x86disassemble(symbols['main']['address'], memoryModel)
 local dotGraph = graph:dotGraph()
---print(dotGraph)
+
+local fh = io.open('test0.dot', 'w')
+fh:write(dotGraph)
+fh:close()
